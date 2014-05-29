b77f519637a79f1f6fb649460afe0be5bf6bb1aa
guanlei <singerinsky@gmail.com>
2014-05-30 06:02:56 +0800

    add code ....

--- common/head.h
+++ common/head.h
@@ -35,6 +35,12 @@
 #include <glog/logging.h>
 #include "../include/tinyxml.h"
 #include "../include/tinystr.h"
+#include <event2/bufferevent.h>
+#include <event2/buffer.h>
+#include <event2/listener.h>
+#include <event2/util.h>
+#include <event2/event.h>
+
 //local
 
 #endif
--- common/reactor_core.h
+++ common/reactor_core.h
@@ -1,12 +1,15 @@
 #include "singleton.h"
+#include "head.h"
 
 class ReactorCore:public Singleton<ReactorCore>
 {
     public:
         friend Singleton<ReactorCore>;
-
-
+    public:
+        bool init();
+        event_base* GetEventBase(){return _base;} 
 
     private:
+        event_base* _base;
 
 };
