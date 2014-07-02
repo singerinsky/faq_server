2e062a7943bc1564868591babbe4b208a1f47fff
singerinsky <singerinsky@gmail.com>
2014-07-02 22:13:27 +0800

    add message_define.h

--- message/CMakeFiles/message_lib.dir/DependInfo.cmake
+++ message/CMakeFiles/message_lib.dir/DependInfo.cmake
@@ -1,7 +1,13 @@
 # The set of languages for which implicit dependencies are needed:
 SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
   )
 # The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gl/faq_server/message/message.pb.cc" "/home/gl/faq_server/message/CMakeFiles/message_lib.dir/message.pb.o"
+  "/home/gl/faq_server/message/role_info.pb.cc" "/home/gl/faq_server/message/CMakeFiles/message_lib.dir/role_info.pb.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
 
 # Targets to which this target links.
 SET(CMAKE_TARGET_LINKED_INFO_FILES
