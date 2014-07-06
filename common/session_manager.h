d4e36686127789b62b140c84203b8a0fe1fbe036
singerinsky <singerinsky@gmail.com>
2014-07-07 07:12:57 +0800

    add new

--- common/session_manager.h
+++ common/session_manager.h
@@ -1,11 +1,37 @@
 #ifndef _SESSION_MANAGER_H_
 #define _SESSION_MANAGER_H_
+#include "singleton.h"
 
-
-class session_manager
+template<class SessionIdType,classs SessionType>
+class session_manager:public Singleton<session_manager>
 {
+    public:
+        friend Singleton<session_manager>;
 
+    public:
+        bool add_session(SessionIdType key,SessionType* value)
+        {
+            auto itr = _session_map.find(key); 
+            if(itr == _session_map.end())
+            {
+                _session_map[key] = value; 
+                return true;
+            }
+            return false;
+        }
 
+        SessionType* get_session(SessionIdType key)
+        {
+            auto itr = _session_map.find(key);
+            if(itr == _session_map.end())
+            {
+                return NULL; 
+            }
+            return itr->second; 
+        }
+
+    private:
+        std::map<SessionIdType,SessionIdType*>  _session_map;
 };
 
 #endif
