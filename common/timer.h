afd0b4ee157027f34fc7ed912342d5788e3cc987
singerinsky <singerinsky@gmail.com>
2014-07-07 17:03:59 +0800

    add new code

--- common/timer.h
+++ common/timer.h
@@ -1,5 +1,7 @@
 #ifndef _TIMER_H_
 #define _TIMER_H_
+#include "head.h"
+#include "reactor_core.h"
 
 void common_timeout_call(evutil_socket_t,short,void *);
 
@@ -15,11 +17,33 @@ template<typename T,void (T::*callback)() = &T::on_timeout>
 class template_timer:public base_timer
 {
     public:
-        template_timer(T* owner = NULL):m_owner(owner){};
+        template_timer(T* owner = NULL):m_owner(owner){
+            m_ev = evtimer_new(Singleton<ReactorCore>::GetInstance()->GetEventBase(),common_timeout_call,(void*)this); 
+        
+        }
+
+        ~template_timer(){}
 
         void set_owner(T* owner)
         {
             m_owner = owner;
+        }
+
+        void set_expire(int64_t ms)
+        {
+            if(m_ev != NULL)
+            {
+               struct timeval tv;
+               evutil_timerclear(&tv);
+               tv.tv_sec = ms/(1000);
+               tv.tv_usec = (ms%1000)*1000;
+               int rst = evtimer_add(m_ev,&tv);             
+               if(rst == -1)
+               {
+                   LOG(ERROR)<<"add timer error"<<ms; 
+               }
+            }
+        
         }
     protected:
         virtual void on_timeout()
@@ -29,6 +53,7 @@ class template_timer:public base_timer
 
     private:
         T* m_owner;
+        struct event* m_ev;
 };
 
 #endif
