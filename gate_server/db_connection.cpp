224d16081255e1dfbe6560f1e932267c5c92a226
gl <singerinsky>
2015-01-21 16:35:59 +0800

    add new code

--- gate_server/db_connection.cpp
+++ gate_server/db_connection.cpp
@@ -37,31 +37,6 @@ void db_connection::on_timeout()
         return; 
     }
       
-//     cs_packet_heart_request request;
-//     process_count++;
-//     request.body.set_client_time(process_count);
-// //    send_packet(&request);
-
-    // cs_packet_db_common_request req;    
-    // req.body.set_operate_type(DbOperateType::MSG_DB_GET_USER_INFO);
-    // req.body.set_operate_string("select * from tb_user");
-    // LOG(INFO)<<process_count<<"send ret ";
-    // int ret = send_packet(&req);
-    // if(ret < 0)
-    // {
-    //      LOG(ERROR)<<"error ";
-    //      return;
-    // }
-
-    // req.body.set_operate_type(DbOperateType::MSG_DB_GET_ITEM_LIST);
-    // req.body.set_operate_string("select * from tb_user_item where bi_ply_guid = 1");
-    // ret = send_packet(&req);
-    // 
-    //  
-    // if(process_count== 10000)
-    //     LOG(INFO)<<"end ";
-   // else
-       // _m_timer.set_expire(1); 
 }
 
 int db_connection::process_db_response(packet_info* info)
